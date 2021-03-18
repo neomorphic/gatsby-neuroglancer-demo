b09e2fd18ec478dbebfed93b76fde4035df98734
Jody Clements <jody@neomorphic.co.uk>
2021-03-18 10:23:05 -0400

    Adds neuroglancer, but icons not working

---
+++ gatsby-node.js
@@ -0,0 +1,19 @@
+/* exports.onCreateWebpackConfig = ({
+  stage,
+  rules,
+  loaders,
+  plugins,
+  actions,
+}) => {
+  actions.setWebpackConfig({
+    module: {
+      rules: [
+        {
+          test: /\.svg$/,
+          loader: require.resolve('svg-inline-loader'),
+          options: {removeSVGTagAttrs: false, removeTags: true}
+        },
+      ],
+    }
+  })
+} */
